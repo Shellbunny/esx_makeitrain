01e9092084f94ee444b8d0ce32e757d7395c0309
Shellbunny <Equifef@gmail.com>
2019-11-22 13:57:55 +1100

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -14,19 +14,21 @@ Citizen.CreateThread(function()
         local plyCoords = GetEntityCoords(PlayerPedId(), 0)
         local distance = #(vector3(Stage.x, Stage.y, Stage.z) - plyCoords)
         if distance < 10 then
-
             if not IsPedInAnyVehicle(PlayerPedId(), true) then
                 if distance < 3 then
                     ESX.Game.Utils.DrawText3D(vector3(Stage.x, Stage.y, Stage.z + 0.5), '[E] Make it rain', 0.4)
-					if IsControlJustReleased(0, 54) then
-
+					if IsControlJustReleased(0, 54) then 
+							
+                                    RequestNamedPtfxAsset("core")
                                     cash = CreateObject(GetHashKey("prop_cash_pile_01"), 0, 0, 0, true, true, true) 
                                     AttachEntityToEntity(cash, GetPlayerPed(-1), GetPedBoneIndex(GetPlayerPed(-1), 18905), 0.12, 0.028, 0.001, 300.00, 180.0, 20.0, true, true, false, true, 1, true)
-
                                     local lib, anim = 'anim@mp_player_intcelebrationfemale@raining_cash', 'raining_cash' do
-                                        Citizen.Wait(900)
-                                        ESX.Streaming.RequestAnimDict(lib, function()
-                                            TaskPlayAnim(PlayerPedId(), lib, anim, 8.0 , -1 , -1 , 0 , 0 , false , false , false);
+                                    Citizen.Wait(900)
+                                    ESX.Streaming.RequestAnimDict(lib, function()
+                                    TaskPlayAnim(PlayerPedId(), lib, anim, 8.0 , -1 , -1 , 0 , 0 , false , false , false);
+                                    Citizen.Wait(1000)
+                                    UseParticleFxAssetNextCall("core")
+                                    local fx = StartParticleFxNonLoopedOnEntity("ent_brk_banknotes", PlayerPedId(), 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, false, false, false)  --(effectName, entity, offsetX, offsetY, offsetZ, rotX, rotY, rotZ, scale, axisX, axisY, axisZ);
                                         end)
                                     end
                                 TriggerServerEvent('esx_makeitrain:pay')
@@ -40,4 +42,3 @@ Citizen.CreateThread(function()
             end
         end
 )
-
